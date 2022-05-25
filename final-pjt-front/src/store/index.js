dc748d70359a972c72c8cda662da4e5d1c9d11b7
minskylee <jewellove1023@gmail.com>
2022-05-25 18:17:00 +0900

    0525 add vuex-persistedstate

--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -5,9 +5,12 @@ import movies from './modules/movies'
 import articles from './modules/articles'
 import accounts from './modules/accounts'
 
+import createPersistedState from "vuex-persistedstate"
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   modules: {
     movies,
     articles,
